1455f724ad72ced94d7596b2683cff745c604808
33345099 <33345099@qq.com>
2014-10-31 23:26:19 +0800

    test

--- docments/src/main/java/com/bestsoft/utils/DateUtils.java
+++ docments/src/main/java/com/bestsoft/utils/DateUtils.java
@@ -4,8 +4,12 @@ import java.util.Calendar;
 import java.util.Date;
 /**
  * author:33345099
+<<<<<<< HEAD
  * 
  */
+=======
+ * /
+>>>>>>> refs/remotes/origin/master
 public class DateUtils {
 	
 	private DateUtils(){
