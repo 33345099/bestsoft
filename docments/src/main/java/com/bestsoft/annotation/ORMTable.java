366c9001d89c5cad9705ab09e8a7b618262320b4
33345099 <33345099@qq.com>
2014-11-06 21:31:16 +0800

    输入注释

---
+++ docments/src/main/java/com/bestsoft/annotation/ORMTable.java
@@ -0,0 +1,13 @@
+package com.bestsoft.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ORMTable {
+	
+	String tableName() default "";
+
+}
